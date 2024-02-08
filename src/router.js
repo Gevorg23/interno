203c31c74c24ae9361eecffea52680e1f70ff3fc
Gevorg23 <138781631+Gevorg23@users.noreply.github.com>
2024-02-08 15:15:58 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from './page/Home.vue';
 import Blog from './page/Blog.vue';
-import Project from './page/Projecct.vue';
+import Project from './page/Project.vue';
 import BlogDetails from './page/BlogDetails.vue';
 import ErrorPage from './page/ErrorPage.vue';
 
